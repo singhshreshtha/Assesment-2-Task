2cb88db8d5a9cc85aec9e0e71ed24e0111826b4f
Shreshtha Singh <shreshthasingh2702@gmail.com>
2020-12-21 00:20:19 +0530

    Remove password from database connection.

--- config/db1.php
+++ config/db1.php
@@ -1,5 +1,5 @@
 <?php
 //echo 'db file called'; 
-$con = mysqli_connect("localhost","root","password123") or die("DB Server is down");
+$con = mysqli_connect("localhost","root","") or die("DB Server is down");
 mysqli_select_db($con,"task") or die("DB not available");
 ?>
